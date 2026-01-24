a538215f2964c23a7788b1839c96b347bff64cb6
Aakash <aakashch.code@gmail.com>
2026-01-24 12:05:02 +0530

    testing url

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE = 'https://intelliwealth-api.onrender.com/api';
+const API_BASE = 'https://intelliwealth-api.onrender.com/api'; //
 const AUTH_BASE = 'https://intelliwealth-api.onrender.com/api';
 
 const api = axios.create({
